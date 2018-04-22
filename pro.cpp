f45435e55deb19bd4be661c59a533793a1f619a8
Martin Krajnak <mkrajnak@redhat.com>
2018-04-22 23:35:47 +0200

    all bad try again

--- pro.cpp
+++ pro.cpp
@@ -33,77 +33,18 @@ int main(int argc, char **argv) {
 
   if (myid == 0) {
     printf("%s\n",argv[1]);
-    size_t n = strlen(argv[1]);
-    size_t array[n+1];
-    size_t alloc = (n/2)*7;
-    char tree[alloc];
-    size_t edge_counter = 0;
-    size_t vertex_counter = 0;
-    for (size_t i = 1; i <= n; i++) {
-      array[i] = argv[1][i-1];
-    }
+    size_t n = strlen(argv[1])+1;
+    char odd[n];
+    char even[n];
+    char str[n];
+    size_t even_counter = 0;
+    size_t odd_counter = 0;
 
-    for (size_t i = 1; i <= (size_t)(n/2); i++) {
-      tree[vertex_counter] = array[i];
-      if ((2*i) <=n ) {
-        tree[vertex_counter+1] = (int)(++edge_counter);
-        tree[vertex_counter+2] = (int)(++edge_counter);
-        tree[vertex_counter+3] = array[2*i];
-      }
-      if ((2*i+1) <=n ) {
-        tree[vertex_counter+4] = (int)(++edge_counter);
-        tree[vertex_counter+5] = (int)(++edge_counter);
-        tree[vertex_counter+6] = array[(2*i)+1];
-      } else {
-        tree[vertex_counter+4] = -1;
-        tree[vertex_counter+5] = -1;
-        tree[vertex_counter+6] = -1;
-      }
-      vertex_counter +=7;
-    }
-    printf("Alloc:%d\n",alloc );
-    for (size_t i = 0; i < alloc; i++) {
-      printf("i:%2d:%2d:\t%c\n",i,tree[i],tree[i]);
-    }
-
-    int next[edge_counter];
-    for (size_t i = 0; i < alloc; i+=7) {
-      if ((i+3) < (int)(alloc/2)) {
-        printf("1.Calculating: %c\n",tree[i+3] );
-        next[tree[i+1]] = (int) tree[i+8];
-        next[tree[i+2]] = (int) tree[i+4];
-      }
-      if ((i+6) < (int)(alloc/2)) {
-        printf("2.Calculating: %c\n",tree[i+6] );
-        if (i+15 < alloc)
-          next[tree[i+4]] = (int) tree[i+15];
-        else
-          next[tree[i+4]] = (int) tree[i+5];
-        next[tree[i+5]] = (int) tree[i+1];
-      }
-      if ((i+3) >= (int)(alloc/2)) {
-        printf("3.Calculating: %c\n",tree[i+3] );
-        next[tree[i+1]] = (int) tree[i+2];
-        if (tree[i+4] == -1 && (i/7)%2 != 0)
-          next[tree[i+2]] = (int) tree[i-5];
-        else if (tree[i+4] == -1)
-          next[tree[i+2]] = (int) tree[i-9];
-        else
-          next[tree[i+2]] = (int) tree[i+4];
-      }
-      if ((i+6) >= (int)(alloc/2)) {
-        printf("4.Calculating: %c\n",tree[i+6] );
-        next[tree[i+4]] = (int) tree[i+5];
-        if (i == 0)
-          next[tree[i+5]] = (int) tree[i+1];
-        else if ((i/7)%2 != 0)
-          next[tree[i+5]] = (int) tree[i-5];
-        else
-          next[tree[i+5]] = (int) tree[i-9];
-      }
-    }
-    for (size_t i = 0; i <= edge_counter; i++) {
-      printf("i:%2d:\t%2d\n",i,next[i]);
+    for (size_t i = 1; i <n ; i++) {
+      str[i] = argv[1][i-1];
+      even[i] = even_counter = even_counter+2;
+      odd[i] = odd_counter = odd_counter+2;
+      printf("Odd:%2d\tEven:%2d\tC:%c\n",odd[i],even[i],str[i]);
     }
 
   }
