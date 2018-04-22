bbce5e6d9e5fb21d26deacb15174355f61db5dee
Martin Krajnak <mkrajnak@redhat.com>
2018-04-22 23:47:23 +0200

    all bad try again

--- pro.cpp
+++ pro.cpp
@@ -33,18 +33,15 @@ int main(int argc, char **argv) {
 
   if (myid == 0) {
     printf("%s\n",argv[1]);
-    size_t n = strlen(argv[1])+1;
-    char odd[n];
-    char even[n];
+    size_t n = strlen(argv[1]);
+    size_t edges_num = 2*n;
+    char edges[edges_num];
     char str[n];
-    size_t even_counter = 0;
-    size_t odd_counter = 0;
 
-    for (size_t i = 1; i <n ; i++) {
-      str[i] = argv[1][i-1];
-      even[i] = even_counter = even_counter+2;
-      odd[i] = odd_counter = odd_counter+2;
-      printf("Odd:%2d\tEven:%2d\tC:%c\n",odd[i],even[i],str[i]);
+    for (size_t i = 1; i <edges_num; i+=2) {
+      edges[i] = i;
+      edges[i+1] = i+1;
+      printf("Odd:%2d\tEven:%2d\n",edges[i],edges[i+1]);
     }
 
   }
