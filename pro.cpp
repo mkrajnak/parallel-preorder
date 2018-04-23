e915060c03852879fd2b9a058f022b0de20addb5
Martin Krajnak <mkrajnak@redhat.com>
2018-04-23 02:43:10 +0200

    Euler tour

--- pro.cpp
+++ pro.cpp
@@ -2,6 +2,7 @@
     Author: Martin Krajnak
 */
 
+#include <math.h>
 #include <mpi.h>
 #include <fstream>
 #include <cstdlib>
@@ -14,12 +15,40 @@
 using namespace std;
 #define TAG 0
 
-int next_left(int i) {
-  return 2*(i+1);
+/**
+* Determine vertex to which given edge leads
+*/
+int get_vertex(int edge){
+  if ((edge%4) == 1) {        // left son vertex
+    return (int)ceil((edge/2.)+1);
+  } else if ((edge%4) == 2) { // parent from left vertex
+    return (int)((edge/4.)+1);
+  } else if ((edge%4) == 3) { // right son vertex
+    return (int)ceil((edge/2.)+1);
+  } else if ((edge%4) == 0) { // parent from right vertex
+    return (int)(edge/4.);
+  }
 }
 
-int next_right(int i) {
-  return 2*(i+1)+1;
+/**
+* Compute the Euler tour
+*/
+int get_next(int edge, int length){
+  int v = get_vertex(edge);
+  if (2*v <= length){
+    if (edge==4)            // special case back to the roots ^^
+      return 1;
+    else if (edge%4 == 0)   // going up
+      return ((edge-2)/2-1);
+    else if (edge%2 == 0 && (2*v+1) <= length) // no right neighbor, go up
+      return edge+1;
+    else if (edge%2 == 0)   // the end
+      return ((edge/2)-1);
+    else
+      return ((v-1)*4+1);   // go down
+  } else {
+    return edge+1;          // go right
+  }
 }
 
 int main(int argc, char **argv) {
@@ -34,15 +63,22 @@ int main(int argc, char **argv) {
   if (myid == 0) {
     printf("%s\n",argv[1]);
     size_t n = strlen(argv[1]);
-    size_t edges_num = 2*n;
-    char edges[edges_num];
-    char str[n];
+    size_t edges_num = 2*n-2;
+    char str[n+1];
+    for (size_t i = 1; i <= n; i++)
+      str[i]=argv[1][i-1];
 
-    for (size_t i = 1; i <edges_num; i+=2) {
+    char edges[edges_num];
+
+    for (size_t i = 1; i < edges_num; i+=2) {
       edges[i] = i;
       edges[i+1] = i+1;
       printf("Odd:%2d\tEven:%2d\n",edges[i],edges[i+1]);
     }
+    for (size_t i = 1; i <= edges_num; i++) {
+      printf("%2d:%2d\n",i, get_next(i,n));
+    }
+
 
   }
   MPI_Finalize();
