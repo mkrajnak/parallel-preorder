b2a3de37af79cd320da434530bcca4fbadd6b152
Martin Krajnak <mkrajnak@redhat.com>
2018-04-23 03:56:48 +0200

    Paralel euler tour

--- pro.cpp
+++ pro.cpp
@@ -36,8 +36,8 @@ int get_vertex(int edge){
 int get_next(int edge, int length){
   int v = get_vertex(edge);
   if (2*v <= length){
-    if (edge==4)            // special case back to the roots ^^
-      return 1;
+    if (edge==4 || (length == 2 && edge==2)) //  I am gR00t
+      return edge;
     else if (edge%4 == 0)   // going up
       return ((edge-2)/2-1);
     else if (edge%2 == 0 && (2*v+1) <= length) // no right neighbor, go up
@@ -60,27 +60,26 @@ int main(int argc, char **argv) {
   MPI_Comm_size(MPI_COMM_WORLD, &numprocs);       // obtain numprocs
   MPI_Comm_rank(MPI_COMM_WORLD, &myid);           // obtain of of each proc
 
+  //printf("%s\n",argv[1]);
+  size_t n = strlen(argv[1]);
+  size_t edges_num = 2*n-2;
+
+  int edges[edges_num];
+
   if (myid == 0) {
-    printf("%s\n",argv[1]);
-    size_t n = strlen(argv[1]);
-    size_t edges_num = 2*n-2;
-    char str[n+1];
-    for (size_t i = 1; i <= n; i++)
-      str[i]=argv[1][i-1];
-
-    char edges[edges_num];
-
-    for (size_t i = 1; i < edges_num; i+=2) {
+    for (size_t i = 0; i < edges_num; i++) { // init edges with numbers
       edges[i] = i;
-      edges[i+1] = i+1;
-      printf("Odd:%2d\tEven:%2d\n",edges[i],edges[i+1]);
+      //printf("%2d:%2d\n",i,edges[i] );
     }
-    for (size_t i = 1; i <= edges_num; i++) {
-      printf("%2d:%2d\n",i, get_next(i,n));
-    }
-
-
   }
+  int edge[1];
+  // obtain the subarray (sub_nums) for every proc
+  MPI_Scatter(edges, 1, MPI_INT, edge, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+  //printf("%2d:E%2d\n",myid, edge[0]);
+  printf("%2d:%2d\n",myid+1, get_next(edge[0]+1,n));
+  //MPI_Gather(edge, 1, MPI_INT, edges, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
   MPI_Finalize();
   return 0;
  }
--- test.sh
+++ test.sh
@@ -5,8 +5,9 @@ if [ $# -lt 1 ];then
     echo "Usage: ./test.sh <String_to_preorder>";
     exit 1;
 fi;
-# cpus=$(echo -n $1 | wc -c);
-cpus=4
+cpus=$(echo -n $1 | wc -c);
+cpus=$(($cpus*2-2))
+echo "cpus:$cpus";
 
 mpic++ --prefix /usr/local/share/OpenMPI -o pro pro.cpp
 # Check if translation was successfull to avoid to many err messages
